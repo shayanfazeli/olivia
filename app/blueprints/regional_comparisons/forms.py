eaee69d03c49bc83421749d28d58f4ef2e9faeb4
Shayan Fazeli <shayan@cs.ucla.edu>
2020-06-06 16:45:50 -0700

    automatic comparisons

--- app/blueprints/regional_comparisons/forms.py
+++ app/blueprints/regional_comparisons/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import TextAreaField, SelectField, SubmitField, DateField
+from wtforms import TextAreaField, SelectField, SubmitField
+from wtforms.fields.html5 import DateField
 from wtforms.validators import DataRequired
 from datetime import date
 import operator
